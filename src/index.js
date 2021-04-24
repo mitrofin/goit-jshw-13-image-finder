31f7fd275d93d854a162927373d792387d857d7a
mitrofin <43621297+mitrofin@users.noreply.github.com>
2021-04-24 15:56:25 +0300

    render card

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import './styles.css';
+import imageCardTmpl from './templates/imageCard.hbs';
 import apiService from './js/apiService';
+import '../node_modules/material-design-icons/iconfont/material-icons.css';
+import * as basicLightbox from 'basiclightbox';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+import getRefs from './js/getrefs';
+import appendMarkUp from './js/appendMarkUp';
+
+const refs = getRefs();
 const api = new apiService();
 
-api.fetchApi();
+api.fetchApi().then(renderCard);
+
+function renderCard({ hits }) {
+  const markUp = imageCardTmpl(hits);
+  appendMarkUp(markUp);
+}
+
+/* const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`);
+
+instance.show(); */
+/* renderCard(); */
---
+++ src/js/getrefs.js
@@ -0,0 +1,8 @@
+export default function getRefs() {
+  return {
+    searchForm: document.querySelector('.search-form'),
+    containerList: document.querySelector('.gallery'),
+    clickButton: document.getElementById('button'),
+    pointForDownloadNextImages: document.querySelector('#dowmload-next-imeges'),
+  };
+}
